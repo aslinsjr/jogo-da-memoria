60ba89b5ed9033d252e2df63b389d2852b9ed066
aslinsjr <aslinsjr@gmail.com>
2024-01-18 08:18:04 -0300

    Final

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
+const reloadBtn = document.querySelector("#reload-btn");
+const gameContainer = document.querySelector(".game");
+const resultContiner = document.querySelector(".result-container");
+const blur = document.querySelector(".blur");
+
 const emojis = [
-"🏍",
-"🏍",
-"🚍",
-"🚍",
-"🚒",
-"🚒",
-"🚑",
-"🚑",
-"✈",
-"✈",
-"🛩",
-"🛩",
-"🚁",
-"🚁",
-"🚀",
-"🚀"
+  "🛩️",
+  "🛩️",
+  "🚍",
+  "🚍",
+  "🚒",
+  "🚒",
+  "🚑",
+  "🚑",
+  "🚴",
+  "🚴",
+  "🏎️",
+  "🏎️",
+  "🚁",
+  "🚁",
+  "🚀",
+  "🚀"
 ];
 
 let openCard = [];
@@ -22,45 +27,47 @@ let openCard = [];
 let radomizeEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
 for (let i = 0; i < emojis.length; i++) {
-    let box = document.createElement("div");
-    box.className = "item"
-    box.innerHTML = radomizeEmojis[i];
-    box.onclick = handleClick;
-    document.querySelector(".game").appendChild(box)
+  let box = document.createElement("div");
+  box.className = "item"
+  box.innerHTML = radomizeEmojis[i];
+  box.onclick = handleClick;
+  gameContainer.appendChild(box)
 }
 
 function handleClick() {
-    if(openCard.length < 2) {
-        this.classList.add("box-open")
-        openCard.push(this)
-    }
+  if (openCard.length < 2) {
+    this.classList.add("box-open")
+    openCard.push(this)
+  }
 
-    if(openCard.length === 2) {
-        setTimeout(checkMatch, 500);
-    }
+  if (openCard.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
 }
 
 function checkMatch() {
-    if(openCard[0].innerHTML === openCard[1].innerHTML) {
+  if (openCard[0].innerHTML === openCard[1].innerHTML) {
 
-        openCard[0].classList.add("box-match");
-        openCard[1].classList.add("box-match");
+    openCard[0].classList.add("box-match");
+    openCard[1].classList.add("box-match");
 
-    } else {
+  } else {
 
-        openCard[0].classList.remove("box-open");
-        openCard[1].classList.remove("box-open");
-    }
+    openCard[0].classList.remove("box-open");
+    openCard[1].classList.remove("box-open");
+  }
 
-    openCard = [];
+  openCard = [];
 
-    if(document.querySelectorAll(".box-match").length === emojis.length) {
-        alert("Venceu!!")
-    }
+  if (document.querySelectorAll(".box-match").length === emojis.length) {
+    // gameContainer.classList.add("hide")
+    resultContiner.classList.remove("hide")
+    blur.classList.add("active")
+  }
 }
 
-let reloadBtn = document.querySelector("#reload-btn");
+
 
 reloadBtn.addEventListener("click", () => {
-    window.location.reload()
+  window.location.reload()
 })
